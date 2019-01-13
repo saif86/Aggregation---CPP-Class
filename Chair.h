2c95e865f1acc48d0a3c8eabbf0be9635613bb27
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-13 22:28:58 +0800

    Update Chair.h

--- Chair.h
+++ Chair.h
@@ -1,13 +1,77 @@
-#ifndef Chair_H
-#define Chair_H
-class Chair
-{
-	bool folded;
+/**  Chair class to demonstrate aggregation in a C++ code.
+*
+* #include "Chair.h" <BR>
+* -llib
+*
+*/
+#ifndef CHAIR_H
+#define CHAIR_H
+
+// SYSTEM INCLUDES
+#include<iostream>
+
+
+// Chair class definition
+class Chair {
 public:
-	Chair();
-	void DoSomething(void);
+// LIFECYCLE
+	/** Default + Overloaded constructor.
+	*/
+	Chair(bool = true);
+
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// Chair(const Chair&);
+	// Chair& operator=(const Chair&);
+	// ~Chair();
+
+
+	
+// OPERATIONS
+
+	/** Does something with the Chair.
+	*
+	* @param void
+	*
+	* @return void
+	*/
+	void DoSomeThing(void);
+
+	/** Folds chair.
+	*
+	* @param void
+	*
+	* @return True if folded successfully, false otherwise.
+	*/
 	bool FoldChair(void);
+
+	/** Unfolds chair.
+	*
+	* @param void
+	*
+	* @return True if unfolded successfully, false otherwise.
+	*/
 	bool UnFoldChair(void);
-	~Chair();
+
+
+// ACCESS
+	// setters
+	void SetFold(bool = true);
+	void SetChair(bool = true);
+	/**
+	# @overload void SetChair(const Chair&);
+	*/
+	void SetChair(const Chair&);
+	
+
+	// getters
+	bool GetFold()const;
+	const Chair& GetChair()const;
+	
+private:
+
+// DATA MEMBERS
+	bool mFolded;
 };
-#endif
+// end class Chair
+#endif  
+// _CHAIR_H_
