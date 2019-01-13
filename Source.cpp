794d42b8198aefe84b24d5c5fababb44e24ac6a7
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-13 22:27:54 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,17 @@
-#include "Room.h"
-
+/**
+* @file Source.cpp
+*
+* @brief This code tests Aggregation of Room class.
+* Room class has aggregation with Chair class.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 #include<iostream>
 using namespace std;
+#include "Room.h"
 
+// function main begins program execution
 void main()
 {
 	Chair ch1;
@@ -12,8 +21,9 @@ void main()
 		r1.AddChair(&ch1, 1);
 		r1.UnFoldChair(1);
 	} // end of nested scope. 
-	//Room (r1) object destroys here but chair (ch1) object remains in memory 
+	  //Room (r1) object destroys here but chair (ch1) object remains in memory 
 	ch1.FoldChair();
 
 	system("pause");
 }
+// end main
