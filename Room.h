0b83cc7d7328db850ae067cd41fd352ca2bf9f1a
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-13 22:31:19 +0800

    Update Room.h

--- Room.h
+++ Room.h
@@ -1,17 +1,86 @@
-#ifndef Room_H
-#define Room_H
+/**  Room class to demonstrate aggregation in a C++ code.
+*
+* #include "Room.h" <BR>
+* -llib
+*
+*/
+#ifndef ROOM_H
+#define ROOM_H
+
+// SYSTEM INCLUDES
+#include<iostream>
+
+// LOCAL INCLUDES
 #include "Chair.h"
-class Room
-{
-	float area;
-	Chair * chairs[50];
+
+// Room class definition
+class Room {
 public:
+// LIFECYCLE
+	/** Default constructor.
+	*/
 	Room();
-	Room(Chair *, int);
-	~Room();
-	void AddChair(Chair *, int);
-	Chair * GetChair(int);
-	bool FoldChair(int);
-	bool UnFoldChair(int);
+
+	/** Overloaded constructor.
+	*/
+	Room(Chair*, int);
+
+	// Use compiler-generated copy constructor, assignment, and destructor.
+	// Room(const Room&);
+	// Room& operator=(const Room&);
+	// ~Room();
+
+
+// OPERATIONS
+	
+	/** Adds chair to the Room.
+	*
+	* @param pChair Pointer to chair to be added.
+	* @param chairNo Chair's location in the room.
+	*
+	* @return void
+	*/
+	void AddChair(Chair* pChair, int chairNo);
+
+	/** Folds chair within the Room.
+	*
+	* @param chairNo Chair's location within the room.
+	*
+	* @return True if folded successfully, false otherwise.
+	*/
+	bool FoldChair(int chairNo);
+
+	/** Unfolds chair within the Room.
+	*
+	* @param chairNo Chair's location within the room.
+	*
+	* @return True if unfolded successfully, false otherwise.
+	*/
+	bool UnFoldChair(int chairNo);
+
+
+// ACCESS
+	// setters
+	void SetArea(float = 0.0);
+	void SetChair(Chair* pChair, int chairNo);
+	void SetRoom(Chair*, int);
+	/**
+	# @overload void SetRoom(const Room&);
+	*/
+	void SetRoom(const Room&);
+
+
+	// getters
+	float GetArea()const;
+	Chair* GetChair(int chairNo)const;
+	const Room& GetRoom()const;
+
+private:
+
+// DATA MEMBERS
+	float mArea;
+	Chair* mpChairs[50];
 };
-#endif
+// end class Room
+#endif  
+// _ROOM_H_
