0d700a6c17d54cd9e078b12cd54a54fb6e3264fb
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-13 22:32:18 +0800

    Update Room.cpp

--- Room.cpp
+++ Room.cpp
@@ -1,54 +1,107 @@
-#include "Room.h"
-#include <iostream>
+#include "Room.h"  // class implemented
 using namespace std;
 
+// File scope starts here 
 
-Room::Room() :area(0.0)
-{
+/////////////////////////////// PUBLIC ///////////////////////////////////////
+
+//============================= LIFECYCLE ====================================
+
+// Room Default Constructor
+Room::Room() : mArea(0.0) {
 	cout << "Room  Default Constructor" << endl;
 	for (int i = 0; i < 50; i++)
-		chairs[i] = NULL;
+		this->mpChairs[i] = NULL;
 }
+// end Room constructor
 
-Room::Room(Chair * chair1, int chairNo)
-{
+// Room Overloaded Constructor
+Room::Room(Chair* pChair, int chairNo) {
 	cout << "Room Overloaded Constructor." << endl;
-	if (chairNo >= 0 && chairNo < 50)
-		chairs[chairNo] = chair1;
+	this->SetRoom(pChair, chairNo);
 }
-Room::~Room()
-{
-	cout << "Room Destructor." << endl;
-}
+// end Room constructor
 
-void Room::AddChair(Chair * chair1, int chairNo){
-	if (chairNo >= 0 && chairNo < 50)
-		chairs[chairNo] = chair1;
-}
 
-Chair * Room::GetChair(int chairNo){
+//============================= OPERATIONS ===================================
+
+// function that adds chair to the Room.
+void Room::AddChair(Chair* pChair, int chairNo) {
+	this->SetChair(pChair, chairNo);
+}
+// end function AddChair
+
+// function that folds chair within the Room.
+bool Room::FoldChair(int chairNo) {
+	cout << "Room is folding chair." << endl;
 	if (chairNo >= 0 && chairNo < 50)
-		return chairs[chairNo];
+		return mpChairs[chairNo]->FoldChair();
+	else
+		return false;
+}
+// end function FoldChair
+
+// function that unfolds chair within the Room.
+bool Room::UnFoldChair(int chairNo) {
+	cout << "Room is unfolding chair." << endl;
+	if (chairNo >= 0 && chairNo < 50)
+		return mpChairs[chairNo]->UnFoldChair();
+	else
+		return false;
+}
+// end function UnFoldChair
+
+
+
+//============================= ACESS      ===================================
+
+// function that sets area of Room
+void Room::SetArea(float aArea) {
+	if (aArea < 0.0)
+		cout << "Error:: Area cannot be nagative." << endl;
+	else
+		this->mArea = aArea;
+}
+// end function SetArea
+
+// function that sets Chair within the Room
+void Room::SetChair(Chair* pChair, int chairNo) {
+	if (chairNo >= 0 && chairNo < 50)
+		mpChairs[chairNo] = pChair;
+}
+// end function SetChair
+
+// function that sets the Room
+void Room::SetRoom(Chair* pChair, int chairNo) {
+	this->SetChair(pChair, chairNo);
+}
+// end function SetRoom
+
+// overloded function that sets the Room
+void Room::SetRoom(const Room& rObj) {
+	this->SetArea(rObj.GetArea());
+	for (int i = 0; i < 50; i++)
+		this->SetRoom(rObj.GetChair(i), i);
+}
+// end function SetRoom
+
+// function that gets area of the Room
+float Room::GetArea()const {
+	return this->mArea;
+}
+// end function GetArea
+
+// function that gets Chair within the Room
+Chair * Room::GetChair(int chairNo)const {
+	if (chairNo >= 0 && chairNo < 50)
+		return mpChairs[chairNo];
 	else
 		return NULL;
 }
+// end function GetChair
 
-
-bool Room::FoldChair(int chairNo)
-{
-	cout << "Room is folding chair." << endl;
-	if (chairNo >= 0 && chairNo < 50)
-		return chairs[chairNo]->FoldChair();
-	else
-		return false;
+// function that get the Room
+const Room& Room::GetRoom()const {
+	return *this;
 }
-	
-
-bool Room::UnFoldChair(int chairNo)
-{
-	cout << "Room is unfolding chair." << endl;
-	if (chairNo >= 0 && chairNo < 50)
-		return chairs[chairNo]->UnFoldChair();
-	else
-		return false;
-}
+// end function GetRoom
